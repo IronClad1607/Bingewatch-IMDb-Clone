70773d72467f6154f40b833b9fdb30e87d83773a
Ishaan Jain <31792878+IronClad1607@users.noreply.github.com>
2019-08-18 13:28:08 +0530

    People Activity Started

--- app/src/main/java/com/ironclad/bingewatch/adapters/movies/CastAdapter.kt
+++ app/src/main/java/com/ironclad/bingewatch/adapters/movies/CastAdapter.kt
@@ -1,11 +1,14 @@
 package com.ironclad.bingewatch.adapters.movies
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.ironclad.bingewatch.R
+import com.ironclad.bingewatch.activities.PeopleActivity
 import com.ironclad.bingewatch.movie_modal.Cast
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.cvcastcrew.view.*
@@ -31,6 +34,11 @@ class CastAdapter(private val cast: ArrayList<Cast>?, val context: Context) :
                 tvName.text = cast.name
                 tvCharacter.text = cast.character
                 Picasso.get().load("https://image.tmdb.org/t/p/w500${cast.profile_path}").fit().into(imagePoster)
+                setOnClickListener {
+                    val peopleIntent = Intent(context, PeopleActivity::class.java)
+                    peopleIntent.putExtra("peopleId", cast.id)
+                    startActivity(context, peopleIntent, null)
+                }
             }
         }
     }
--- app/src/main/java/com/ironclad/bingewatch/network/PeopleAPI.kt
+++ app/src/main/java/com/ironclad/bingewatch/network/PeopleAPI.kt
@@ -1,4 +1,21 @@
 package com.ironclad.bingewatch.network
 
+import com.ironclad.bingewatch.movie_modal.ImageResponse
+import com.ironclad.bingewatch.movie_modal.PeopleCredits
+import com.ironclad.bingewatch.movie_modal.PeopleDetails
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Path
+
 interface PeopleAPI {
+
+    @GET("/3/person/{person_id}?api_key=d5b568462e39f02e011bb612583ead1e&language=en-US")
+    suspend fun getDetails(@Path("person_id") person_id: Int): Response<PeopleDetails>
+
+    @GET("/3/person/{person_id}/combined_credits?api_key=d5b568462e39f02e011bb612583ead1e&language=en-US")
+    suspend fun getCombinedCredits(@Path("person_id")person_id: Int):Response<PeopleCredits>
+
+    @GET("/3/person/{person_id}/images?api_key=d5b568462e39f02e011bb612583ead1e")
+    suspend fun getImages(@Path("person_id")person_id: Int):Response<ImageResponse>
+
 }
